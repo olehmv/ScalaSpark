14c813cb7f88b8e45be49343e9f889cc36814c94
olehmv <oleg19850405@hotmail.com>
2017-12-04 16:55:30 +0200

    collect auto data by brand in HashMap[String,Dataset[Row]]

--- src/main/scala/AutoDriver.scala
+++ src/main/scala/AutoDriver.scala
@@ -1,40 +1,40 @@
 // Import Spark SQL
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{DataTypes, TimestampType}
+import org.apache.log4j.Logger
+import org.apache.spark.sql.functions._
 //https://www.jetbrains.com/help/idea/eclipse.html
 object AutoDriver {
 
-  def main(args: Array[String]): Unit = {
-    val hiveSql = SparkSession.builder().appName("Auto bazara").master("local[*]").enableHiveSupport().getOrCreate()
-    import hiveSql.implicits._
-    import org.apache.spark.sql.types.{DateType, DoubleType, StringType, StructField, StructType}
-    val aschema = StructType(Array(
-      StructField("dateCrawled", DataTypes.TimestampType),
-      StructField("name", StringType),
-      StructField("seller", StringType),
-      StructField("offerType", StringType),
-      StructField("price", DoubleType),
-      StructField("abtest",StringType),
-      StructField("vehicleType",StringType),
-      StructField("yearOfRegistration",DoubleType),
-      StructField("gearbox",StringType),
-      StructField("powerPS",DoubleType),
-      StructField("model",StringType),
-      StructField("kilometer",DoubleType),
-      StructField("monthOfRegistration",DoubleType),
-      StructField("fuelType",StringType),
-      StructField("brand",StringType),
-      StructField("notRepairedDamage",StringType),
-      StructField("dateCreated",DataTypes.TimestampType),
-      StructField("nrOfPictures",DoubleType),
-      StructField("postalCode",DoubleType),
-      StructField("lastSeen",DataTypes.TimestampType)
 
-    ))
+  object AutoDriver {
 
-    val autoData2 = hiveSql.read.schema(aschema).options(Map("header"->"true","dateCrawled"->"yyyy-MM-dd HH:mm:ss","dateCreated"->"yyyy-MM-dd HH:mm:ss","lastSeen"->"yyyy-MM-dd HH:mm:ss")).csv("autos.csv")
-    autoData2.show()
+    def main(args: Array[String]): Unit = {
+      val log = Logger.getLogger(getClass.getName)
+      import org.apache.spark.sql.{DataFrame, Dataset, Row}
+      import org.apache.spark.sql.types.{DateType, DoubleType, StringType, StructField, StructType}
+
+      import scala.collection.immutable.HashMap
+      val hiveSql = SparkSession.builder().appName("Auto bazara").master("local[*]").enableHiveSupport().getOrCreate()
 
 
+      val autoData0: DataFrame = hiveSql.read.format("com.databricks.spark.csv").options(Map("header" -> "true", "inferSchema" -> "true", "dateCrawled" -> "yyyy-MM-dd HH:mm:ss", "dateCreated" -> "yyyy-MM-dd 45", "lastSeen" -> "yyyy-MM-dd HH:mm:ss")).load("autos.csv").cache()
+      val brandList = autoData0.select("brand").distinct().collectAsList();
+      val brandIterator = brandList.iterator()
+      var brandMap = scala.collection.mutable.HashMap[String, Dataset[Row]]()
+      var rowCount: Long = 0
+      var mapValueCount: Long = 0
+      while (brandIterator.hasNext) {
+        var brandName: String = brandIterator.next().toString().replaceAll("\\]", "").replaceAll("\\[", "").trim
+        var brandDataset: Dataset[Row] = autoData0.filter(autoData0.col("brand") === brandName)
+        brandMap += brandName -> brandDataset
+        brandMap(brandName).show(1)
+        rowCount = brandDataset.count()
+        mapValueCount = brandMap(brandName).count()
+        if (rowCount != mapValueCount) {
+          print("Warning rowCount != mapValueCount Warning rowCount != mapValueCount Warning rowCount != mapValueCount")
+          log.info("Warning rowCount != mapValueCount Warning rowCount != mapValueCount Warning rowCount != mapValueCount")
+        }
+      }
+    }
   }
 }
