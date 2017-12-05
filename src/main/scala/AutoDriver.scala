451455352b29027bd1c080b8b4b61bab322cbab5
olehmv <oleg19850405@hotmail.com>
2017-12-05 17:04:48 +0200

    add PieChart

--- src/main/scala/AutoDriver.scala
+++ src/main/scala/AutoDriver.scala
@@ -2,30 +2,52 @@
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.JavaConverters._
+import scalafx.application.JFXApp
+
 //https://www.jetbrains.com/help/idea/eclipse.html
 //-Xms1336m -Xmx1336m
-object AutoDriver {
+object AutoDriver extends JFXApp {
 
-  def main(args: Array[String]): Unit = {
-    val log = Logger.getLogger(getClass.getName)
-    import org.apache.spark.sql.{DataFrame, Dataset, Row}
-    val hiveSql = SparkSession.builder().appName("Auto bazara").master("local[*]").enableHiveSupport().getOrCreate()
-    hiveSql.sparkContext.setLogLevel("WARN")
-    val autoData0: DataFrame = hiveSql.read.format("com.databricks.spark.csv").options(Map("header" -> "true", "inferSchema" -> "true", "dateCrawled" -> "yyyy-MM-dd HH:mm:ss", "dateCreated" -> "yyyy-MM-dd 45", "lastSeen" -> "yyyy-MM-dd HH:mm:ss")).load("autos.csv")
-    val brandList = autoData0.select("brand").distinct().collectAsList()
-    print(brandList)
-    var brandDataset: Dataset[Row] = null
-    val brandIterator = brandList.iterator()
-    while (brandIterator.hasNext) {
-      var brandName: String = brandIterator.next().toString().replaceAll("\\]", "").replaceAll("\\[", "").trim
-      if (brandName != "null") {
-        brandDataset = autoData0.filter(autoData0.col("brand") === brandName)
-        brandDataset.createOrReplaceTempView(brandName)
-        brandDataset.sqlContext.sql(s"select * from $brandName").show()
+
+  val log = Logger.getLogger(getClass.getName)
+
+  import org.apache.spark.sql.{DataFrame, Dataset, Row}
+
+  val hiveSql = SparkSession.builder().appName("Auto bazara").master("local[*]").enableHiveSupport().getOrCreate()
+  hiveSql.sparkContext.setLogLevel("WARN")
+  val autoData0: DataFrame = hiveSql.read.format("com.databricks.spark.csv").options(Map("header" -> "true", "inferSchema" -> "true")).load("autos.csv")
+  val brandList = autoData0.select("brand").distinct().collectAsList()
+  var dataPairss = scala.collection.mutable.HashMap[String, Long]()
+  var brandDataset: Dataset[Row] = null
+  val brandIterator = brandList.iterator()
+  while (brandIterator.hasNext) {
+    var brandName: String = brandIterator.next().toString().replaceAll("\\]", "").replaceAll("\\[", "").trim
+    if (brandName != "null") {
+      brandDataset = autoData0.filter(autoData0.col("brand") === brandName)
+      brandDataset.createOrReplaceTempView(brandName)
+      dataPairss += brandName -> brandDataset.sqlContext.sql(s"select * from $brandName").count()
+    }
+  }
+  stage = new JFXApp.PrimaryStage {
+
+    import scalafx.scene.Scene
+
+    title = "Auto Pie Chart "
+    scene = new Scene (500,500){
+
+      import scalafx.scene.chart.PieChart
+
+      root = new PieChart {
+
+        import scalafx.collections.ObservableBuffer
+
+        title = "Auto Pie Chart"
+        clockwise = false
+        data = ObservableBuffer(dataPairss.toList.map { case (x, y) => PieChart.Data(x, y) })
       }
     }
-
-    hiveSql.stop()
   }
+  hiveSql.stop()
 }
 
