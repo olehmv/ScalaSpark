2c3ede67cee585c4e2a7480fe46dfe6294473c15
olehmv <oleg19850405@hotmail.com>
2017-12-05 16:16:39 +0200

    registate sql tables by brand

--- src/main/scala/AutoDriver.scala
+++ src/main/scala/AutoDriver.scala
@@ -1,43 +1,31 @@
 // Import Spark SQL
-import org.apache.spark.sql.SparkSession
 import org.apache.log4j.Logger
-import org.apache.spark.sql.functions._
-//https://www.jetbrains.com/help/idea/eclipse.html
+import org.apache.spark.sql.SparkSession
 
+//https://www.jetbrains.com/help/idea/eclipse.html
+//-Xms1336m -Xmx1336m
 object AutoDriver {
 
   def main(args: Array[String]): Unit = {
     val log = Logger.getLogger(getClass.getName)
     import org.apache.spark.sql.{DataFrame, Dataset, Row}
-    import org.apache.spark.sql.types.{DateType, DoubleType, StringType, StructField, StructType}
-
-    import scala.collection.immutable.HashMap
     val hiveSql = SparkSession.builder().appName("Auto bazara").master("local[*]").enableHiveSupport().getOrCreate()
-//    hiveSql.sparkContext.setLogLevel("WARN")
-
-    val autoData0: DataFrame = hiveSql.read.format("com.databricks.spark.csv").options(Map("header" -> "true", "inferSchema" -> "true", "dateCrawled" -> "yyyy-MM-dd HH:mm:ss", "dateCreated" -> "yyyy-MM-dd 45", "lastSeen" -> "yyyy-MM-dd HH:mm:ss")).load("autos.csv").cache()
-    val brandList = autoData0.select("brand").distinct().collectAsList();
-    var brandDataset: Dataset[Row]=null
+    hiveSql.sparkContext.setLogLevel("WARN")
+    val autoData0: DataFrame = hiveSql.read.format("com.databricks.spark.csv").options(Map("header" -> "true", "inferSchema" -> "true", "dateCrawled" -> "yyyy-MM-dd HH:mm:ss", "dateCreated" -> "yyyy-MM-dd 45", "lastSeen" -> "yyyy-MM-dd HH:mm:ss")).load("autos.csv")
+    val brandList = autoData0.select("brand").distinct().collectAsList()
+    print(brandList)
+    var brandDataset: Dataset[Row] = null
     val brandIterator = brandList.iterator()
     while (brandIterator.hasNext) {
       var brandName: String = brandIterator.next().toString().replaceAll("\\]", "").replaceAll("\\[", "").trim
-      if(brandName==null){
-       print(brandName)
-        return
+      if (brandName != "null") {
+        brandDataset = autoData0.filter(autoData0.col("brand") === brandName)
+        brandDataset.createOrReplaceTempView(brandName)
+        brandDataset.sqlContext.sql(s"select * from $brandName").show()
       }
-      brandDataset= autoData0.filter(autoData0.col("brand") === brandName)
-      if(brandDataset==null){
-        brandDataset.show(1)
-      }
-      brandDataset.registerTempTable(brandName)
-//      print(brandDataset.select(s"select count(brand) from $brandName"))
     }
-    brandDataset.select("SELECT kilometer FROM volvo").show(2)
-//    autoData0.filter(autoData0.col("brand") === "jaguar").show()
+
+    hiveSql.stop()
   }
 }
-//17/12/04 17:21:12 INFO SparkSqlParser: Parsing command: jaguar
-//17/12/04 17:21:12 INFO SparkSqlParser: Parsing command: daihatsu
-//17/12/04 17:21:12 INFO SparkSqlParser: Parsing command: mitsubishi
-//17/12/04 17:21:12 INFO SparkSqlParser: Parsing command: null
-//17/12/04 17:21:12 INFO SparkSqlParser: Parsing command: lada
+
